cffead1aca5adb7dcc202c5ce8e4fd7528513d01
Azizah <113151140+zahelo@users.noreply.github.com>
2024-10-28 02:39:18 +0700

    Hello Universe

---
+++ no-1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var i int
+	fmt.Println("Input Angka: ")
+	fmt.Scan(&i)
+
+	if i == 42 {
+		println("Hello Universe")
+	} else {
+		println(i)
+	}
+}
